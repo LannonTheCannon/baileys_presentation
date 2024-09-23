568c7c89c8cfaeb0ee56ced9feb0553b1bd30307
EpicRed1000 <epicred1000@gmail.com>
2024-09-23 16:52:16 -0700

    Added Streamlit Extras

--- src/chatbot_expanded.py
+++ src/chatbot_expanded.py
@@ -1,5 +1,6 @@
 # chatbot_expanded.py
 import streamlit as st
+from streamlit_extras.stoggle import stoggle
 from src.material_game import material_game
 
 
@@ -8,11 +9,15 @@ def display_chatbot_expanded():
     st.title(":green-background[What can this chatbot do for you]")
     st.subheader("Image it's 6 pm. You just finished designing plans for your next big project. You're ready to fall asleep on your nice comfortable bed... but suddently your boss marches him and forces you figure out the necessary materials for the project. Tired and defeated you groan as you work for the next couple hours reaserching and comparing diffrent materials, never getting to go home, never getting to your bed...")
     st.write("")
-    if st.button("But...",key = game_start):
-        st.subheader("This Ai allows you to compare and contrast diffrent materials in minutes.")
-        st.subheader("The Ai gives out information about a wide range of materials using the given critera and prompt.")
-        st.subheader("For example...")
-        material_game()
+##    if st.button("But...",key = game_start):
+##        st.subheader("This Ai allows you to compare and contrast diffrent materials in minutes.")
+##        st.subheader("The Ai gives out information about a wide range of materials using the given critera and prompt.")
+##        st.subheader("For example...")
+##        material_game()
+    stoggle("But...",
+        '''This Ai allows you to compare and contrast diffrent materials in minutes.
+        The Ai gives out information about a wide range of materials using the given critera and prompt.'''
+            )
         
         # companies used materials
         # vn esque
